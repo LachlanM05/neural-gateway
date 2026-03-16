a6fdcb892439e9b7a2824adae36d421f741f2346
LachlanM05 <lachlanm05@gmail.com>
2026-03-17 05:41:59 +1100

    fix versioning appearence in GUI electron app

--- client/electron-app/preload.js
+++ client/electron-app/preload.js
@@ -4,5 +4,6 @@ contextBridge.exposeInMainWorld('api', {
     toggleConnection: (creds) => ipcRenderer.invoke('toggle-connection', creds),
     toggleStartup: (enabled) => ipcRenderer.invoke('toggle-startup', enabled),
     toggleStats: (enabled) => ipcRenderer.invoke('toggle-stats', enabled),
-    onStatus: (callback) => ipcRenderer.on('status-update', (event, val) => callback(val))
+    onStatus: (callback) => ipcRenderer.on('status-update', (event, val) => callback(val)),
+    getVersion: () => ipcRenderer.invoke('get-version')
 });
